324fa96fecafe8b04b385fd8832511e86e46c4ae
daeyeongjeon <eodudsla10@naver.com>
2022-04-26 13:50:50 +0900

    solve : 220426 시험 성적

---
+++ src/BOJ/BOJ9498.java
@@ -0,0 +1,23 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ9498 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        if (90 <= a &&  a<= 100) {
+            System.out.println("A");
+        } else if (80 <= a && a <= 89) {
+            System.out.println("B");
+        } else if (70 <= a && a <= 79) {
+            System.out.println("C");
+        } else if (60 <= a && a <= 69) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
