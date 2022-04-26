3238d102d41ddef1a4ea05664e7d7e1bce8ee729
daeyeongjeon <eodudsla10@naver.com>
2022-04-26 13:51:55 +0900

    solve : 220426 사분면 고르기

---
+++ src/BOJ/BOJ14681.java
@@ -0,0 +1,24 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ14681 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        if (a < 0 && b > 0) {
+            System.out.println(2);
+        } else if (a > 0 && b > 0) {
+            System.out.println(1);
+        } else if (a < 0 && b < 3) {
+            System.out.println(3);
+        } else if (a > 0 && b < 4) {
+            System.out.println(4);
+        }
+    }
+}
