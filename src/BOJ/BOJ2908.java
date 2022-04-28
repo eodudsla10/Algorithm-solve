fe8d6228e0d21ee74e9cdb403b28a67d96e43c66
daeyeongjeon <eodudsla10@naver.com>
2022-04-28 11:08:17 +0900

    solve : 220428 상수

---
+++ src/BOJ/BOJ2908.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2908 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        a = Integer.parseInt(new StringBuilder().append(a).reverse().toString());
+        b = Integer.parseInt(new StringBuilder().append(b).reverse().toString());
+
+        System.out.println(a > b ? a : b);
+    }
+
+}
