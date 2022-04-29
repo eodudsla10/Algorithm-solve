83598862890d3256ae45982ca9c2c0450d12d9e7
daeyeongjeon <eodudsla10@naver.com>
2022-04-29 14:20:54 +0900

    solve : 220428 짝수와 홀수

---
+++ src/PRO/PRO12937.java
@@ -0,0 +1,13 @@
+package PRO;
+
+public class PRO12937 {
+    public String solution(int num) {
+        String answer = "";
+        if (num % 2 == 0) {
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
