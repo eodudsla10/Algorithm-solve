ea3b3b3d90a70129a1fbf624202f74dc852b6549
daeyeongjeon <eodudsla10@naver.com>
2022-05-04 13:20:56 +0900

    solve : 220504 시저 암호

---
+++ src/PRO/PRO12926.java
@@ -0,0 +1,33 @@
+package PRO;
+
+public class PRO12926 {
+    public static void main(String[] args) {
+        String s = "z";
+        int n = 1;
+        System.out.println(solution(s,n));
+    }
+    public static String solution(String s, int n) {
+        String answer = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                answer += c;
+                continue;
+            }
+            if ('a' <= c && c <= 'z') {
+                if (c+n > 'z') {
+                    answer += (char)(c -26+n);
+                } else {
+                    answer += (char)(c + n);
+                }
+            } else if ('A' <= c && c <= 'Z') {
+                if (c+n > 'Z') {
+                    answer += (char)(c -26+n);
+                } else {
+                    answer += (char)(c + n);
+                }
+            }
+        }
+        return answer;
+    }
+}
